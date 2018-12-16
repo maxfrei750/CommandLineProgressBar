3232d4786a323234beae7f9887e1cadb197b9ff8
Max Frei <max.frei@uni-due.de>
2018-12-16 20:43:14 +0100

    Adept to the previous commit.

--- demo_CommandLineProgressBar_parfor.m
+++ demo_CommandLineProgressBar_parfor.m
@@ -1,21 +1,16 @@
-%% Setup parallel pool.
-setupparallelpool
-dataQueue = parallel.pool.DataQueue;
-
 %% Setup progress bar.
 N = 30;
 progressBar = CommandLineProgressBar(N);
 
 % Optional settings.
-progressBar.message = 'Status';
+progressBar.message = 'Status: ';
 progressBar.barLength = 42;
 
 %% Loop
-afterEach(dataQueue, @(varargin) increment(progressBar));
 parfor idx = 1:N
     % Work
     pause(rand());
     
     % Update progressbar.
-    send(dataQueue,idx);
+    progressBar.increment;
 end
